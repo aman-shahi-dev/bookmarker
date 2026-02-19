264a47611a06ff42d978de3ac91a85ca8fef0cce
Aman Shahi <amanshahidev@gmail.com>
2026-02-19 18:50:12 +0530

    added speed insights from vercel

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,11 +3,13 @@ import { Footer } from "../components/Footer";
 import { ToastContainer } from "react-toastify";
 import { Outlet } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export const Layout = () => {
   return (
     <div className="font-inter flex min-h-screen w-full flex-col bg-[#1A1A1A] text-white">
       <Analytics />
+      <SpeedInsights />
       <Navbar />
       <main className="flex flex-1 flex-col">
         <Outlet />
