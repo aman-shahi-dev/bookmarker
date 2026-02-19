b4e2563071a90a7bf647e5205d13b8a53716e148
Aman Shahi <amanshahidev@gmail.com>
2026-02-19 09:45:12 +0530

    updated homepage and added icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,30 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../services/appwrite/appwrite.js";
 import { logout } from "../store/authSlice";
+import { IconUser, IconLogout } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
 
 export const Navbar = () => {
   const { status, userData, loading } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [showProfile, setShowProfile] = useState(false);
+  const profileDropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        profileDropdownRef.current &&
+        !profileDropdownRef.current.contains(event.target)
+      ) {
+        setShowProfile(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   const handleLogout = () => {
     logoutUser()
@@ -27,12 +46,35 @@ export const Navbar = () => {
         <div className="h-8 w-24 animate-pulse rounded-full bg-neutral-800"></div>
       ) : status ? (
         <div className="flex items-center justify-center gap-4">
-          <div className="p-4 rounded-full bg-yellow-400"></div>
+          <div className="relative" ref={profileDropdownRef}>
+            <div
+              onClick={() => setShowProfile((prev) => !prev)}
+              className="flex cursor-pointer flex-col rounded-full bg-yellow-400 p-2 text-black hover:bg-yellow-500 active:scale-98"
+            >
+              <IconUser size={24} />
+            </div>
+            {showProfile && (
+              <div
+                onClick={(e) => e.stopPropagation()}
+                className="absolute top-12 right-0 z-50 min-w-50 rounded-lg border border-neutral-700 bg-yellow-400 p-2 text-black shadow-2xl select-auto"
+              >
+                <h1 className="text-md text-center font-bold underline md:text-xl">
+                  Account
+                </h1>
+                <h2 className="text-center text-sm md:text-lg">
+                  {userData?.name}
+                </h2>
+                <h2 className="text-center text-sm md:text-lg">
+                  {userData?.email}
+                </h2>
+              </div>
+            )}
+          </div>
           <button
             onClick={handleLogout}
-            className="cursor-pointer rounded-full bg-red-500 px-3 py-1 hover:bg-red-700 active:scale-95"
+            className="cursor-pointer rounded-full bg-red-500 p-2 select-none hover:bg-red-700 active:scale-95"
           >
-            Logout
+            <IconLogout size={24} />
           </button>
         </div>
       ) : (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
 
   if (loading)
     return (
-      <h1 className="bg-black text-white flex flex-1 animate-pulse flex-col items-center justify-center text-2xl md:text-4xl">
+      <h1 className="flex flex-1 animate-pulse flex-col items-center justify-center bg-black text-2xl text-white md:text-4xl">
         Loading...
       </h1>
     );
@@ -16,18 +16,23 @@ export const Home = () => {
     <div className="flex flex-1 flex-col items-center justify-start">
       {status ? (
         <>
-          <h1 className="text-2xl text-yellow-400 md:text-6xl mt-20 mb-6">
+          <h1 className="mt-20 mb-6 text-2xl font-bold tracking-tight text-yellow-400 md:text-6xl">
             Welcome {userData?.name.split(" ")[0]} !
           </h1>
           <PlaylistActions />
         </>
       ) : (
         <>
-          <h1 className="text-2xl text-yellow-400 text-shadow-lg md:text-6xl mt-35 mb-6">
-            Welcome to Bookmarker !
+          <h1 className="mt-35 mb-6 text-2xl font-bold tracking-tight text-yellow-400 text-shadow-lg md:text-6xl">
+            Stop Scrolling. Start Learning
           </h1>
+          <h2 className="mt-2 max-w-xl px-10 text-center text-lg tracking-tight text-neutral-400 md:text-xl">
+            Turn any messy YouTube playlist into a beautiful, structured course
+            in one click. Organize your learning, track your progress, and stay
+            focused.
+          </h2>
           <Link
-            to="/login"
+            to="/signup"
             className="mt-10 rounded-full bg-yellow-400 bg-linear-to-br px-6 py-2 text-xl text-black transition duration-300 hover:bg-yellow-500 active:scale-95 md:px-10 md:py-4 md:text-2xl"
           >
             Try it now
