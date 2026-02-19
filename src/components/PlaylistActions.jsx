4cdc2fdfcc210ebcbcf511460aa318215331e3b7
Aman Shahi <amanshahidev@gmail.com>
2026-02-19 09:50:27 +0530

    fixed playlist action buttons size

--- src/components/PlaylistActions.jsx
+++ src/components/PlaylistActions.jsx
@@ -3,18 +3,18 @@ import { IconPlaylist, IconPlaylistAdd } from "@tabler/icons-react";
 
 export const PlaylistActions = () => {
   return (
-    <div className="mt-10 flex h-15 w-80 items-center justify-evenly gap-4 md:w-120">
+    <div className="mt-10 flex flex-col items-center justify-evenly gap-4 md:w-120 md:flex-row">
       <Link
         to="/generate-playlist"
-        className="flex h-full w-1/2 items-center justify-center rounded-full bg-yellow-400 px-2 text-sm font-bold text-black hover:bg-yellow-500 active:scale-95 md:text-xl gap-2"
+        className="flex items-center justify-center gap-2 rounded-full bg-yellow-400 px-6 py-4 text-sm font-bold text-black hover:bg-yellow-500 active:scale-95 md:text-xl"
       >
-        <IconPlaylistAdd/> Generate Playlist
+        <IconPlaylistAdd /> Generate Playlist
       </Link>
       <Link
         to="/my-playlists"
-        className="flex h-full w-1/2 items-center justify-center rounded-full bg-yellow-400 px-2 text-sm font-bold text-black hover:bg-yellow-500 active:scale-95 md:text-xl gap-2"
+        className="flex items-center justify-center gap-2 rounded-full bg-yellow-400 px-6 py-4 text-sm font-bold text-black hover:bg-yellow-500 active:scale-95 md:text-xl"
       >
-        <IconPlaylist/> My Playlists
+        <IconPlaylist /> My Playlists
       </Link>
     </div>
   );
