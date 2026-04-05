653bedc612d7de0777cf39869e158bd515302a0d
Aman Shahi <amanshahidev@gmail.com>
2026-04-05 20:33:41 +0530

    fixed button alignment

--- src/pages/MyPlaylists.jsx
+++ src/pages/MyPlaylists.jsx
@@ -24,7 +24,7 @@ export const MyPlaylists = () => {
         </div>
         <Link
           to="/"
-          className="bg-btn hover:bg-hover right-10 mx-auto mt-3 w-fit rounded-md px-3 py-1 text-black active:scale-95 md:mt-10"
+          className="bg-btn hover:bg-hover right-10 mx-auto mt-10 w-fit rounded-md px-3 py-1 text-black active:scale-95"
         >
           Back
         </Link>
